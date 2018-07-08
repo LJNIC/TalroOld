dd4077b5836e9049cbd2c6b4e1b71de1b84b3e06
Liam <liamreadsspam@gmail.com>
2018-07-08 12:31:38 -0300

    Add rat entity

---
+++ entities/rat.lua
@@ -0,0 +1,13 @@
+--Rats are small, quick, and travel in packs.
+Rat = {}
+
+function Rat:new(x, y, symbol, fg, bg, map)
+	r = Entity:new(x, y, symbol, fg, bg, map)
+	r.type = 'rat'
+	return r
+end
+
+function Rat:ai()
+	--TODO Rat AI. They should travel in packs and attack the player but run away frequently.
+end
+
