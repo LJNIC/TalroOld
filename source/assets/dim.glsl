2beebc54121d313df5d00152fbd8b4415b240232
Liam <liamreadsspam@gmail.com>
2019-02-20 20:44:46 -0400

    Dim pause more, new header image

--- source/assets/dim.glsl
+++ source/assets/dim.glsl
@@ -1,4 +1,4 @@
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
 	vec4 pixel = Texel(texture, texture_coords );
-	return vec4(pixel.r * 0.5, pixel.g * 0.5, pixel.b * 0.5, 1.0);
+	return vec4(pixel.r * 0.3, pixel.g * 0.3, pixel.b * 0.3, 1.0);
 }
